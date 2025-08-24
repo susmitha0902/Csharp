3dd2ed16f66f30e7181df2c79979a6f24363b3e1
Susmitha Raparthi <susmithar@infinite.com>
2025-08-24 15:58:24 +0530

    MVC Assignment1 updated

---
+++ MVC/Assignments/Assignment-1/Assignment-1/Controllers/ContactsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Assignment_1.Models;
+using Assignment_1.Repositories;
+
+namespace Assignment_1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactRepository _repository;
+
+        public ContactController()
+        {
+            _repository = new ContactRepository();
+        }
+
+
+        public async Task<ActionResult> Index()
+        {
+            var contacts = await _repository.GetAllAsync();
+            return View(contacts);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.CreateAsync(contact);
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+
+        public async Task<ActionResult> Delete(long id)
+        {
+            var contacts = await _repository.GetAllAsync();
+            var contact = contacts.Find(c => c.Id == id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Assignments/Assignment-1/Assignment-1/Repositories/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment_1.Models;
+namespace Assignment_1.Repositories
+{
+    interface IContactRepository
+    {
+        Task<List<Contact>> GetAllAsync();
+        Task CreateAsync(Contact contact);
+        Task DeleteAsync(long Id);
+    }
+}
